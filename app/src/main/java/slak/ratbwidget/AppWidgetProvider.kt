3a6f4081ba556d783d0d56e540a7bf5168a1d888
Stefan Silviu <sylviu44@gmail.com>
2019-03-05 19:57:20 +0200

    Avoid passing around Context instances

--- app/src/main/java/slak/ratbwidget/AppWidgetProvider.kt
+++ app/src/main/java/slak/ratbwidget/AppWidgetProvider.kt
@@ -40,14 +40,14 @@ class RATBWidgetProvider : AppWidgetProvider() {
   }
 
   /** Update the [RemoteViews] content using the provided [Schedule]. */
-  private fun showSchedule(context: Context, views: RemoteViews, schedule: Schedule) {
+  private fun Context.showSchedule(views: RemoteViews, schedule: Schedule) {
     fun buildTime(moment: Moment): String = "${padNr(moment.t / 100)}:${padNr(moment.t % 100)}"
     val now = ZonedDateTime.now(ZoneId.systemDefault())
     val currentMoment = Moment(now.hour * 100 + now.minute)
     val moments = schedule.pickList(now.dayOfWeek).let {
       if (it != null) return@let it
-      views.setTextViewText(R.id.prevTime, context.resources.getString(R.string.not_available))
-      views.setTextViewText(R.id.nextTime, context.resources.getString(R.string.not_available))
+      views.setTextViewText(R.id.prevTime, resources.getString(R.string.not_available))
+      views.setTextViewText(R.id.nextTime, resources.getString(R.string.not_available))
       return@showSchedule
     }!!.flatten()
     val nextIdx = moments.indices.firstOrNull { idx -> currentMoment < moments[idx] } ?: 0
@@ -56,100 +56,99 @@ class RATBWidgetProvider : AppWidgetProvider() {
     val next2 = buildTime(moments[next2Idx])
     val prevIdx = if (nextIdx == 0) moments.size - 1 else nextIdx - 1
     val prev = buildTime(moments[prevIdx])
-    views.setTextViewText(R.id.prevTime, context.resources.getString(R.string.prev_time, prev))
-    views.setTextViewText(R.id.nextTime, context.resources.getString(R.string.next_time, next, next2))
+    views.setTextViewText(R.id.prevTime, resources.getString(R.string.prev_time, prev))
+    views.setTextViewText(R.id.nextTime, resources.getString(R.string.next_time, next, next2))
   }
 
   /** Update the [RemoteViews] content using the provided [Route]. */
-  private fun showRoute(context: Context, views: RemoteViews, route: Route, reverse: Boolean) {
+  private fun Context.showRoute(views: RemoteViews, route: Route, reverse: Boolean) {
     val stops = if (reverse) route.stopsFrom else route.stopsTo
-    val routeText = context.resources.getString(R.string.route, stops.first().name, stops.last().name)
+    val routeText = resources.getString(R.string.route, stops.first().name, stops.last().name)
     views.setTextViewText(R.id.route, routeText)
   }
 
   /** Create an [Intent] based on the given parameters and set it as the click pending intent for the view at [id]. */
-  private fun buildIntent(context: Context,
-                          views: RemoteViews,
-                          appWidgetId: Int,
-                          @IdRes id: Int,
-                          use: (Intent) -> Unit = {}) {
-    val intent = Intent(context, this@RATBWidgetProvider::class.java)
+  private fun Context.buildIntent(views: RemoteViews,
+                                  appWidgetId: Int,
+                                  @IdRes id: Int,
+                                  use: (Intent) -> Unit = {}) {
+    val intent = Intent(this, this@RATBWidgetProvider::class.java)
     intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, intArrayOf(appWidgetId))
     use(intent)
     val reqCode = reqCodes()
     Log.v(TAG, "Building intent for $appWidgetId, reqCode=$reqCode")
     views.setOnClickPendingIntent(id,
-        PendingIntent.getBroadcast(context, reqCode, intent, PendingIntent.FLAG_UPDATE_CURRENT))
+        PendingIntent.getBroadcast(this, reqCode, intent, PendingIntent.FLAG_UPDATE_CURRENT))
   }
 
-  override fun onReceive(context: Context, intent: Intent) {
+  override fun onReceive(context: Context, intent: Intent): Unit = with(context) {
     val id = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS)?.get(0)
     Log.v(TAG, "Received broadcast: id=$id, action=${intent.action}")
     when (intent.action) {
       ACTION_TOGGLE_DIR -> {
-        context.p.toggleIsReverse(id!!, context.p.lineNr(id))
+        p.toggleIsReverse(id!!, p.lineNr(id))
         updateWidget(context, AppWidgetManager.getInstance(context), id)
       }
       ACTION_LINE_CHANGE -> {
-        val newIntent = context.intentFor<PhonyDialog>()
+        val newIntent = intentFor<PhonyDialog>()
         newIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_NEW_TASK)
         newIntent.action = ACTION_SELECT_LINE
         newIntent.putExtra(EXTRA_WIDGET_ID, id)
-        context.startActivity(newIntent)
+        startActivity(newIntent)
       }
       ACTION_STOP_CHANGE -> {
-        val newIntent = context.intentFor<PhonyDialog>()
+        val newIntent = intentFor<PhonyDialog>()
         newIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_NEW_TASK)
         newIntent.action = ACTION_SELECT_STOP
         newIntent.putExtra(EXTRA_WIDGET_ID, id)
         newIntent.putParcelableArrayListExtra(EXTRA_STOP_LIST, intent.getParcelableArrayListExtra(EXTRA_STOP_LIST))
-        context.startActivity(newIntent)
+        startActivity(newIntent)
       }
       ACTION_SHOW_ALL_SCHEDULE -> {
-        val newIntent = context.intentFor<ViewScheduleActivity>()
+        val newIntent = intentFor<ViewScheduleActivity>()
         newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         newIntent.putExtra(EXTRA_WIDGET_ID, id)
-        context.startActivity(newIntent)
+        startActivity(newIntent)
       }
       else -> super.onReceive(context, intent)
     }
   }
 
-  private fun updateWidget(context: Context, appWidgetManager: AppWidgetManager, id: Int) {
+  private fun updateWidget(context: Context, appWidgetManager: AppWidgetManager, id: Int) = with(context) {
     Log.i(TAG, "Updating widget $id")
-    val views = RemoteViews(context.packageName, R.layout.initial_layout)
+    val views = RemoteViews(packageName, R.layout.initial_layout)
 
-    buildIntent(context, views, id, R.id.refreshBtn) { it.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE }
-    buildIntent(context, views, id, R.id.lineNumber) { it.action = ACTION_LINE_CHANGE }
-    buildIntent(context, views, id, R.id.swapDirBtn) { it.action = ACTION_TOGGLE_DIR }
-    buildIntent(context, views, id, R.id.allMomentsBtn) { it.action = ACTION_SHOW_ALL_SCHEDULE }
+    buildIntent(views, id, R.id.refreshBtn) { it.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE }
+    buildIntent(views, id, R.id.lineNumber) { it.action = ACTION_LINE_CHANGE }
+    buildIntent(views, id, R.id.swapDirBtn) { it.action = ACTION_TOGGLE_DIR }
+    buildIntent(views, id, R.id.allMomentsBtn) { it.action = ACTION_SHOW_ALL_SCHEDULE }
 
     // Defaults
-    views.setTextViewText(R.id.prevTime, context.resources.getString(R.string.prev_time, "?"))
-    views.setTextViewText(R.id.nextTime, context.resources.getString(R.string.next_time, "?", "?"))
-    views.setTextViewText(R.id.route, context.resources.getString(R.string.route, "?", "?"))
+    views.setTextViewText(R.id.prevTime, resources.getString(R.string.prev_time, "?"))
+    views.setTextViewText(R.id.nextTime, resources.getString(R.string.next_time, "?", "?"))
+    views.setTextViewText(R.id.route, resources.getString(R.string.route, "?", "?"))
 
-    val line = context.p.lineNr(id)
+    val line = p.lineNr(id)
     views.setTextViewText(R.id.lineNumber, line.nr.toString())
 
     GlobalScope.launch(Dispatchers.IO) {
       val route = getRoute(line) ?: return@launch
       Log.v(TAG, "getRoute($line): $route")
 
-      val stops = if (context.p.isReverse(id, line)) route.stopsFrom else route.stopsTo
-      val targetStop = stops.find { it.stopId == context.p.stopId(id, line) } ?: stops[0]
+      val stops = if (p.isReverse(id, line)) route.stopsFrom else route.stopsTo
+      val targetStop = stops.find { it.stopId == p.stopId(id, line) } ?: stops[0]
 
       val schedule = getSchedule(route, targetStop) ?: return@launch
       Log.v(TAG, "getSchedule(<route>, $targetStop): $schedule")
 
       withContext(Dispatchers.Main) {
-        showRoute(context, views, route, context.p.isReverse(id, line))
+        showRoute(views, route, p.isReverse(id, line))
         views.setTextViewText(R.id.stop, targetStop.name)
-        buildIntent(context, views, id, R.id.stop) {
+        buildIntent(views, id, R.id.stop) {
           it.action = ACTION_STOP_CHANGE
           it.putParcelableArrayListExtra(EXTRA_STOP_LIST, ArrayList(stops))
         }
-        showSchedule(context, views, schedule)
+        showSchedule(views, schedule)
       }
 
       appWidgetManager.updateAppWidget(id, views)
